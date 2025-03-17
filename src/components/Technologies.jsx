ced7b63e79f365fee439f31e2132bfee9d3b719c
SaifAbdelrazek011 <saifamr1234567@gmail.com>
2025-03-18 01:16:45 +0200

    Initial Source

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,85 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+} from "react-icons/si";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+function Technologies() {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-6 flex justify-center"
+        >
+          <SiReact className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-6 flex justify-center"
+        >
+          <SiHtml5 className="text-7xl text-orange-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-6 flex justify-center"
+        >
+          <SiCss3 className="text-7xl text-blue-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-6 flex justify-center"
+        >
+          <SiJavascript className="text-7xl text-yellow-300" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-6 flex justify-center"
+        >
+          <SiTypescript className="text-7xl text-blue-500" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default Technologies;
